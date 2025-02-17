b7dec6e9cb8b81f5e48ad33e86960ad1047a5d30
josephteh97 <1079183153@qq.com>
2025-02-17 18:32:56 +0800

    adjust file names

--- environment.py
+++ environment.py
@@ -2,6 +2,17 @@ import isaacgym
 from isaacgym import gymapi, gymtorch
 import torch
 
+class Config:
+    def __init__(self):
+        self.state_dim = 24
+        self.action_dim = 4
+        self.learning_rate = 0.001
+        self.gamma = 0.99
+        self.episodes = 1000
+        self.model_path = "robot_arm_model.pth"
+        self.dt = 0.02
+        self.initial_state = [0, 0, 0, 0]  # Example initial state, modify as needed
+
 class WashbasinCleaningEnv:
     def __init__(self, config):
         self.config = config
